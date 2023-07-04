342f7a18cf59d066dce0b0d8166c9b71789a60f7
zengproof <84146255+zengproof@users.noreply.github.com>
2023-07-04 12:45:23 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 18 - https://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="font-awesome.min.css" rel="stylesheet">
+<link href="liah_new.css" rel="stylesheet">
+<link href="contact.css" rel="stylesheet">
+<script src="jquery-3.6.0.min.js"></script>
+<script src="wb.panel.min.js"></script>
+<script src="imagemapresizer.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   $("#PanelMenu1").panel({animate: true, animationDuration: 200, animationEasing: 'linear', dismissible: true, display: 'overlay', position: 'right', toggle: true, overlay: true});
+   $('#wb_ImageHotspots1 map').imageMapResize();
+   $('#wb_ImageHotspots3 map').imageMapResize();
+});
+</script>
+</head>
+<body>
+<div id="wb_LayoutGrid1">
+<div id="LayoutGrid1">
+<div class="row">
+<div class="col-1">
+<div id="wb_PageHeader">
+<div id="PageHeader">
+<div class="row">
+<div class="col-1">
+<picture id="wb_Picture1">
+<img src="images/LIAH LOGO NEW 1.jpg" id="Picture1" alt="" width="165" height="165">
+</picture>
+</div>
+<div class="col-2">
+<div id="wb_Heading1">
+<h1 id="Heading1">LIAH FOODS</h1>
+</div>
+</div>
+<div class="col-3">
+<div id="wb_PanelMenu1">
+<a href="#PanelMenu1_markup" id="PanelMenu1">&nbsp;</a>
+<div id="PanelMenu1_markup">
+<ul role="menu">
+<li role="menuitem"><a href="index.html" class="nav-link"><i class="fa fa-home fa-fw"></i><span>Home</span></a></li>
+<li role="menuitem"><a href="services.html" class="nav-link"><i class="fa fa-leaf fa-fw"></i><span>Services</span></a></li>
+<li role="menuitem"><a href="shop.html" class="nav-link"><i class="fa fa-shopping-bag fa-fw"></i><span>Shop</span></a></li>
+<li role="menuitem"><a href="about.html" class="nav-link"><i class="fa fa-user fa-fw"></i><span>About</span></a></li>
+<li role="menuitem"><a href="contact.php" class="nav-link"><i class="fa fa-volume-control-phone fa-fw"></i><span>Contact</span></a></li>
+</ul>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+<div id="wb_ImageHotspots1">
+<img src="images/index_17.png" id="ImageHotspots1" alt="" usemap="#ImageHotspots1-map" width="1440" height="240">
+<map name="ImageHotspots1-map">
+</map>
+</div>
+<picture id="wb_Picture4">
+<img src="images/index_0.png" id="Picture4" alt="" width="1170" height="84">
+</picture>
+<div id="wb_ImageHotspots3">
+<img src="images/index_066.jpg" id="ImageHotspots3" alt="" usemap="#ImageHotspots3-map" width="1440" height="540">
+<map name="ImageHotspots3-map">
+<area shape="rect" id="ImageHotspots3-hotspot-0" coords="30,356,259,429" href="https://www.facebook.com/100089301133458" target="" alt="" title="">
+</map>
+</div>
+<picture id="wb_Picture2">
+<img src="images/index_0.png" id="Picture2" alt="" width="1170" height="84">
+</picture>
+</div>
+</div>
+</div>
+</div>
+<div id="wb_contactForm">
+<form name="feedback" method="post" action="https://formspree.io/f/mleyrkwn" enctype="multipart/form-data" accept-charset="UTF-8" target="_blank" id="contactForm">
+<div class="row">
+<div class="col-1">
+<div id="wb_contactHeading">
+<h2 id="contactHeading">Get In Touch</h2>
+</div>
+<div id="wb_contactBreadcrumb1">
+<ul id="contactBreadcrumb1">
+<li><a href=""><i class="fa fa-map-marker">&nbsp;</i>P.O Box 645, Bellville, Cape Town</a></li>
+</ul>
+
+</div>
+<div id="wb_contactBreadcrumb2">
+<ul id="contactBreadcrumb2">
+<li><a href="http://www.yourwebsite.com/"><i class="fa fa-phone">&nbsp;</i></a></li>
+</ul>
+
+</div>
+<div id="wb_contactBreadcrumb3">
+<ul id="contactBreadcrumb3">
+<li><a href=""><i class="fa fa-envelope-o">&nbsp;</i>liahplantbased@gmail.com</a></li>
+</ul>
+
+</div>
+</div>
+<div class="col-2">
+<div class="col-2-padding">
+<div id="wb_Heading13">
+<h2 id="Heading13">Mail Us</h2>
+</div>
+<div id="wb_LayoutGrid4">
+<form name="feedback" method="post" action="https://formspree.io/f/mleyrkwn" enctype="multipart/form-data" target="_blank" id="LayoutGrid4">
+<div class="row">
+<div class="col-1">
+<input type="text" id="Editbox1" name="name" value="" spellcheck="false" placeholder="Name">
+<input type="text" id="Editbox2" name="email" value="" spellcheck="false" placeholder="Email">
+<textarea name="comments" id="TextArea1" rows="5" cols="95" spellcheck="false" placeholder="Message"></textarea>
+<input type="submit" id="Button1" name="" value="Send">
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</form>
+</div>
+<div id="wb_LayoutGrid2">
+<form name="contact" method="post" action="https://formspree.io/f/mleyrkwn" enctype="multipart/form-data" id="LayoutGrid2">
+<div class="row">
+<div class="col-1">
+</div>
+</div>
+</form>
+</div>
+<div id="wb_LayoutGrid9">
+<div id="LayoutGrid9">
+<div class="row">
+<div class="col-1">
+<div id="wb_Text8">
+<span style="color:#FFFFFF;font-family:Arial;font-size:13px;">© 2018 Liah foods. All Rights Reserved.</span>
+</div>
+</div>
+<div class="col-2">
+<label for="" id="Label4">Follow us:</label>
+<div id="wb_FontAwesomeIcon5">
+<a href="https://www.facebook.com/profile.php?id=100089301133458"><div id="FontAwesomeIcon5"><i class="fa fa-facebook-f"></i></div></a>
+</div>
+<div id="wb_FontAwesomeIcon6">
+<div id="FontAwesomeIcon6"><i class="fa fa-twitter"></i></div>
+</div>
+<div id="wb_FontAwesomeIcon9">
+<a href="https://www.instagram.com/liahfoods/"><div id="FontAwesomeIcon9"><i class="fa fa-instagram"></i></div></a>
+</div>
+<div id="wb_FontAwesomeIcon8">
+<a href="https://www.youtube.com/@liahfoods"><div id="FontAwesomeIcon8"><i class="fa fa-youtube"></i></div></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
